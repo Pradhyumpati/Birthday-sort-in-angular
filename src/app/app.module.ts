5d306f661d6fd2231633d33d4d8c5dccbf34b012
Pradhyum Patidar <pradhyump777@gmail.com>
2023-01-28 19:24:29 +0530

    Initialcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { Child1Component } from './child1/child1.component';
+import { Child2Component } from './child2/child2.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Child1Component,
+    Child2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
